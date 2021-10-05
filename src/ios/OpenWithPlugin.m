345515bee2b3851040ab04efd64c8a55ae0c39a5
guijesus86 <34098957+guijesus86@users.noreply.github.com>
2021-10-05 20:40:23 +0100

    Update OpenWithPlugin.m

--- src/ios/OpenWithPlugin.m
+++ src/ios/OpenWithPlugin.m
@@ -1,9 +1,6 @@
 #import <Cordova/CDV.h>
 #import <MobileCoreServices/MobileCoreServices.h>
-
-#define SHAREEXT_GROUP_IDENTIFIER @"__GROUP_IDENTIFIER__"
-#define SHAREEXT_URL_SCHEME @"__URL_SCHEME__"
-#define SHAREEXT_UNIFORM_TYPE_IDENTIFIER @"__UNIFORM_TYPE_IDENTIFIER__"
+#import "ShareViewController.h"
 
 /*
  * Add base64 export to NSData
